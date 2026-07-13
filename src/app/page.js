a096ad85b6515b3c8849a7941317991f38b4f52c
chirzin633 <chirzin.attabiq@gmail.com>
2026-07-13 15:51:40 +0700

    init project

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="text-center text-slate-800 font-bold text-4xl">
+          Hello World!
+        </div>
+      </div>
+    </>
+  );
+}
